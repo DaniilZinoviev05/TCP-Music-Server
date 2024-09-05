708c04c968bd02730f6f87c15cc63061de5cc77c
danielzinoviev@yandex.ru <danielzinoviev@yandex.ru>
2024-09-05 23:10:42 +0300

    Server class update

--- server.h
+++ server.h
@@ -3,19 +3,43 @@
 class Server 
 {
 public:
-	Server(){
-		mk_socket();
+	Server(struct sockaddr_in sock_addr){
+		sock_address = sock_addr; 
 	}
-
-private:
-	int socket_d; // дескриптор сокета
-	struct sockaddr_in sock_addr; 
 
 	void mk_socket(){
 		socket_d = socket(AF_INET, SOCK_STREAM, 0);
 		if (socket_d < 0){
-			std::cerr << "Не удалось создать сокет.";
+			std::cerr << "Не удалось создать сокет.\n";
 		}	
-		std::cout << "Сокет успешно создан.";
+		std::cout << "Сокет успешно создан.\n";
 	}
+
+	void bind_socket(){
+		int b_socket = bind(socket_d, (struct sockaddr*) &sock_address, sizeof(sock_address));
+		if (b_socket < 0){
+			std::cerr << "Не удалось привязать сокет.\n";
+		}	
+		std::cout << "Сокет успешно привязан.\n";
+	}
+
+	void listen_socket(){
+		int l_socket = listen(socket_d, 5);
+		if (l_socket < 0){
+			std::cerr << "Ошибка при прослушивании.\n";
+		}	
+		std::cout << "Сервер начал прослушивать, порт: " << PORT << std::endl;
+	}
+
+	void connection(){
+		int conn = accept(socket_d, (struct sockaddr*) &sock_address, (socklen_t *)(sizeof(sock_address)));
+		if (conn < 0){
+			std::cerr << "Ошибка соединения.\n";
+		}	
+		std::cout << "Соединение установлено." << std::endl;
+	}
+
+private:
+	int socket_d; // дескриптор сокета
+	struct sockaddr_in sock_address;
 };
--- socket.h
+++ socket.h
@@ -3,4 +3,6 @@
 	#include <iostream>
 	#include <sys/socket.h>
 	#include <netinet/in.h>
+
+	#define PORT 8585
 #endif
